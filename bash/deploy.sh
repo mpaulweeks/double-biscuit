3c85c0666c6e14a0706db92a2459a45f679c8220
mpaulweeks <mpaulweeks@gmail.com>
2017-11-11 18:32:55 -0500

    create react app

---
+++ bash/deploy.sh
@@ -0,0 +1,19 @@
+cd tetris-app
+
+npm run build
+
+mkdir ../docs
+touch ../docs/CNAME
+cp ../docs/CNAME CNAME.temp
+rm -r ../docs
+
+mkdir ../docs
+cp -a build/ ../docs
+cp CNAME.temp ../docs/CNAME
+rm CNAME.temp
+cp README_BUILD.md ../docs/README.md
+
+cd ..
+git add .
+git commit -m '[deploy]'
+git push
