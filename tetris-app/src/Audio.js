1e2368b63ce95a32cad796a8103e81f94ad62981
mpaulweeks <mpaulweeks@gmail.com>
2017-11-20 12:50:30 -0500

    song vs clip

--- tetris-app/src/Audio.js
+++ tetris-app/src/Audio.js
@@ -1,21 +1,43 @@
 
-class AudioClip(src){
-  constructor(){
-    this.ready = false;
+class AudioSong(){
+  constructor(src){
+    this.loaded = false;
     this.audio = new Audio();
     this.audio.src = src;
 
     const self = this;
     this.audio.addEventListener('canplaythrough', function(){
-       self.ready = true;
+       self.loaded = true;
     });
   }
 
   play(){
     this.audio.play();
   }
+
+  ready(){
+    return loaded;
+  }
+}
+
+class AudioClip(){
+  constructor(src, bandwidth){
+    this.clips = [];
+    for (var i = 0; i < bandwidth; i++){
+      this.clips.push(new AudioSong(src));
+    }
+  }
+
+  play(){
+    for (let c of this.clips){
+      if (c.ready()){
+        return c.play();
+      }
+    }
+  }
 }
 
 export {
+  AudioSong,
   AudioClip,
 }
