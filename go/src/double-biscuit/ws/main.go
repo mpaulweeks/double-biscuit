3b871147a63acbb90c53020de40a84bc82c12d3a
mpaulweeks <mpaulweeks@gmail.com>
2017-11-27 16:16:37 -0500

    add healthcheck

--- go/src/double-biscuit/ws/main.go
+++ go/src/double-biscuit/ws/main.go
@@ -2,18 +2,15 @@ package main
 
 import (
 	"flag"
+	"io"
 	"log"
 	"net/http"
 )
 
 var addr = flag.String("addr", ":5080", "http service address")
 
-func serveHome(w http.ResponseWriter, r *http.Request) {
+func serveExample(w http.ResponseWriter, r *http.Request) {
 	log.Println(r.URL)
-	if r.URL.Path != "/" {
-		http.Error(w, "Not found", 404)
-		return
-	}
 	if r.Method != "GET" {
 		http.Error(w, "Method not allowed", 405)
 		return
@@ -21,11 +18,21 @@ func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "home.html")
 }
 
+func serveRoot(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/health", http.StatusFound)
+}
+
+func serveHealth(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "ok")
+}
+
 func main() {
 	flag.Parse()
 	hub := newHub()
 	go hub.run()
-	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/example", serveExample)
+	http.HandleFunc("/", serveRoot)
+	http.HandleFunc("/health", serveHealth)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
