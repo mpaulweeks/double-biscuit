83b95d2ba50623c2594b8faf7868db5698b9c27f
mpaulweeks <mpaulweeks@gmail.com>
2018-02-14 18:43:32 -0500

    update deploy script

--- tetris-app/bash/deploy.sh
+++ tetris-app/bash/deploy.sh
@@ -6,10 +6,9 @@ cp ../docs/CNAME CNAME.temp
 rm -r ../docs
 
 mkdir ../docs
-cp -a build/ ../docs
+cp -a build/. ../docs/
 cp CNAME.temp ../docs/CNAME
 rm CNAME.temp
-cp README_BUILD.md ../docs/README.md
 
 cd ..
 git add .
